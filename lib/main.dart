53fbc64c90b7d97ccaa295e44690e6e05b9c6b50
Sugiono <1772037@maranatha.ac.id>
2020-05-14 16:09:38 +0700

    change style, update carousel, add animation, search page

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:plant_store/items.dart';
+import 'package:plant_store/pages.dart';
+import 'package:plant_store/view/item_page.dart';
+import 'package:plant_store/view/login_page.dart';
+import 'package:plant_store/view/profile_page.dart';
+import 'package:plant_store/view/register_page.dart';
+import 'package:plant_store/view/search_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-import 'package:plant_store/login.dart';
-import 'package:plant_store/main_page.dart';
-import 'package:plant_store/profile.dart';
-import 'package:plant_store/register.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,15 +17,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MainApp(),
-      routes: {
-        "/main": (context) => MainPage(),
+
+    var routes = {
+        "/main": (context) => Pages(),
         "/profile": (context) => ProfilePage(),
         "/register": (context) => RegisterPage(),
         "/login": (context) => LoginPage(),
         "/items": (context) => ItemPage(),
+        "/search": (context) => SearchPage(),
+      };
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          color: Colors.green,
+        ),
+        primaryColor: Colors.green,
+        accentColor: Colors.greenAccent,
+        bottomAppBarTheme: BottomAppBarTheme(
+          color: Colors.green
+        ),
+        textTheme: TextTheme()
+      ),
+      home: MainApp(),
+
+      onGenerateRoute: (settings) {
+        if(settings.name == "/items"){
+          return CupertinoPageRoute(builder: (context) => ItemPage(), settings: settings);
+        }
+
+        if(settings.name == "/search"){
+          return PageRouteBuilder(
+            pageBuilder: (_,__,___) => SearchPage(),
+            transitionsBuilder: (_, animation, __, child) {
+              return FadeTransition(opacity: animation, child: child);
+            },
+            settings: settings,
+          );
+        }
+        return MaterialPageRoute(builder: (context) => routes[settings.name](context), settings: settings);
       },
     );
   }
